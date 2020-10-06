b19a8ca112739a3615adfa5c49e3a19192b52149
EvanBeaudoin <evan.beaudoin@ocsbstudent.ca>
2020-10-06 02:47:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Let's calculate the area of a rectangle")
+
+Top_length = int(input("Enter the Top Length of trapezoid "))
+Bottom_Length = int(input("Enter the Bottom Length of trapezoid "))
+Height = int(input("Enter the height of trapezoid "))
+
+area = ((Top_length + Bottom_Length) /2) * Height
+
+print("Area of your Trapezoid is:", area,"cm2")
